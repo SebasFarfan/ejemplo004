5fa859b1c17ce1cbfd1fc4e2c5e213ce72f0927e
sebas <sebas.654@gmail.com>
2023-04-20 01:45:02 -0300

    agregó proyecto spring boot

---
+++ tp02_grupoxx/src/main/java/ar/edu/unju/fi/tp02_grupoxx/Tp02GrupoxxApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp02_grupoxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp02GrupoxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp02GrupoxxApplication.class, args);
+	}
+
+}
---
+++ tp02_grupoxx/src/test/java/ar/edu/unju/fi/tp02_grupoxx/Tp02GrupoxxApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp02_grupoxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp02GrupoxxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
